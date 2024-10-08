443cb1950989ade868e21f63a0290e82e7492986
kINo <kinobhu@outlook.com>
2024-10-08 20:55:43 +0800

    Idiotic coding crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
-use std::io::{stdin, Read, Result};
-use crossterm::terminal::enable_raw_mode;
+use crossterm::event::Event;
+use crossterm::event::KeyCode;
+use crossterm::event::KeyEventKind;
+use crossterm::terminal;
+use crossterm::event;
 
-fn main() -> Result<()> {
-    enable_raw_mode().expect("Crossterm: failed to init raw mode!");
-
-    let reader = stdin();
-
-    for b in reader.bytes() {
-        let b = b.expect("Read: failed to read byte!");
-        let c = b as char;
-
-        if c.is_control() {
-            println!("Binary: {0:08b} ASCII: {0:#03}", b);
-        } else {
-            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}", b, c);
+fn main() {
+    terminal::enable_raw_mode().unwrap();
+    loop {
+        let e = event::read().unwrap();
+        if let Event::Key(key_event) = e {
+            if key_event.kind == KeyEventKind::Press {
+                if let KeyCode::Char(c) = key_event.code {
+                    let b = c as u8;
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+                    if c == 'q' {
+                        break;
+                    }
+                }
+            }
         }
-
-        if c == 'q' {
-            enable_raw_mode().expect("Crossterm: failed to exit raw mode!");
-            break;
-        }
-    };
-
-    Ok(())
+    }
+    terminal::disable_raw_mode().unwrap();
 }
