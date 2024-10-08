309f180f2b1e8c5a6c71daa8abbbfce3f6ee72c7
kINo <kinobhu@outlook.com>
2024-10-08 17:08:10 +0800

    Raw mode & read chars

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdin, Read, Result};
+use crossterm::terminal::enable_raw_mode;
+
+fn main() -> Result<()> {
+    enable_raw_mode().expect("Crossterm: failed to init raw mode!");
+
+    let reader = stdin();
+
+    for b in reader.bytes() {
+        let b = b.expect("Read: failed to read byte!");
+        let c = b as char;
+
+        if c.is_control() {
+            println!("Binary: {0:08b} ASCII: {0:#03}", b);
+        } else {
+            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}", b, c);
+        }
+
+        if c == 'q' {
+            enable_raw_mode().expect("Crossterm: failed to exit raw mode!");
+            break;
+        }
+    };
+
+    Ok(())
 }
